82843ca2f8cb4842515433685c0f74c01f19da9b
Shameek Nath <shameek_nath@harvard.edu>
2015-08-12 11:26:24 -0400

    update s3 bucket name

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -14,7 +14,7 @@
 # permissions and limitations under the License.
 
 # ELB_LIST defines which Elastic Load Balancers this instance should be part of.
-ELB_LIST=""
+ELB_LIST="devops-bootcam-Elb-1943GK19EJ2WV"
 
 # Under normal circumstances, you shouldn't need to change anything below this line.
 # -----------------------------------------------------------------------------
